0a924d655daa4028300f68131eb0e04d45e07670
J Scott Smith <jscsmith@gmail.com>
2017-02-01 14:36:31 -0800

    add Home link in SiteNav

--- src/components/SiteNav/SiteNav.jsx
+++ src/components/SiteNav/SiteNav.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import { Link } from 'react-router';
+import { IndexLink, Link } from 'react-router';
 
 export default function SiteNav() {
     return (
         <nav>
             <h3>Site Navigation</h3>
             <ul>
+                <li><IndexLink to="/">Home</IndexLink></li>
                 <li><Link to="/about">About</Link></li>
             </ul>
         </nav>
