91bda9c2958cef82efea38cc4af58a87f02292fb
J Scott Smith <jscsmith@gmail.com>
2017-02-01 17:33:38 -0800

    use import instead of require

--- src/containers/Home/Home.jsx
+++ src/containers/Home/Home.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import Helmet from 'react-helmet';
-const styles = require('./Home.scss');
+import styles from './Home.scss';
 
 export default class Home extends Component {
     render() {
