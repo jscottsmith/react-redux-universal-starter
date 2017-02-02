bfb536bdeb5c062532f943d1b3ffb202f5dc3375
J Scott Smith <jscsmith@gmail.com>
2017-02-01 20:50:41 -0800

    ignore `no-return-assign` in ApiClient

--- src/helpers/ApiClient.js
+++ src/helpers/ApiClient.js
@@ -13,27 +13,30 @@ function formatUrl(path) {
     return '/api' + adjustedPath;
 }
 
+/* eslint-disable no-return-assign */
 export default class ApiClient {
     constructor(req) {
         methods.forEach((method) =>
-      this[method] = (path, { params, data } = {}) => new Promise((resolve, reject) => {
-          const request = superagent[method](formatUrl(path));
+            this[method] = (path, { params, data } = {}) => new Promise((resolve, reject) => {
+                const request = superagent[method](formatUrl(path));
 
-          if (params) {
-              request.query(params);
-          }
+                if (params) {
+                    request.query(params);
+                }
 
-          if (__SERVER__ && req.get('cookie')) {
-              request.set('cookie', req.get('cookie'));
-          }
+                if (__SERVER__ && req.get('cookie')) {
+                    request.set('cookie', req.get('cookie'));
+                }
 
-          if (data) {
-              request.send(data);
-          }
+                if (data) {
+                    request.send(data);
+                }
 
-          request.end((err, { body } = {}) => err ? reject(body || err) : resolve(body));
-      }));
+                request.end((err, { body } = {}) => err ? reject(body || err) : resolve(body));
+            }));
     }
+/* eslint-disable no-return-assign */
+
   /*
    * There's a V8 bug where, when using Babel, exporting classes with only
    * constructors sometimes fails. Until it's patched, this is a solution to
