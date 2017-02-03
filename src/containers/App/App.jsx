55f0030e48df3223d7b5e26f08b742fb01ab1b5c
J Scott Smith <jscsmith@gmail.com>
2017-02-02 16:21:16 -0800

    more cleanup of unused stuff

--- src/containers/App/App.jsx
+++ src/containers/App/App.jsx
@@ -1,11 +1,8 @@
 import React, { Component, PropTypes } from 'react';
-import { connect } from 'react-redux';
 import { IndexLink } from 'react-router';
 import Helmet from 'react-helmet';
 import { isLoaded as isInfoLoaded, load as loadInfo } from 'redux/modules/info';
-import { isLoaded as isAuthLoaded, load as loadAuth, logout } from 'redux/modules/auth';
 import { InfoBar, SiteNav } from 'components';
-import { push } from 'react-router-redux';
 import config from '../../config';
 import { asyncConnect } from 'redux-async-connect';
 
@@ -20,44 +17,19 @@ import '../../sass/styles.scss';
         if (!isInfoLoaded(getState())) {
             promises.push(dispatch(loadInfo()));
         }
-        if (!isAuthLoaded(getState())) {
-            promises.push(dispatch(loadAuth()));
-        }
 
         return Promise.all(promises);
     },
 }])
 
-@connect(
-    state => ({ user: state.auth.user }),
-    { logout, pushState: push })
-
 export default class App extends Component {
 
     static propTypes = {
         children: PropTypes.object.isRequired,
-        logout: PropTypes.func.isRequired,
-        pushState: PropTypes.func.isRequired,
-        user: PropTypes.object,
     };
 
     static contextTypes = {
         store: PropTypes.object.isRequired,
-    };
-
-    componentWillReceiveProps(nextProps) {
-        if (!this.props.user && nextProps.user) {
-            // login
-            this.props.pushState('/loginSuccess');
-        } else if (this.props.user && !nextProps.user) {
-            // logout
-            this.props.pushState('/');
-        }
-    }
-
-    handleLogout = (event) => {
-        event.preventDefault();
-        this.props.logout();
     };
 
     render() {
